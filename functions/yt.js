09876b8696c449e86590f7598145fe2f10d5f3af
Natsuki <69230730+Natsuki26@users.noreply.github.com>
2022-10-01 18:26:43 +0700

    Update yt.js

--- functions/yt.js
+++ functions/yt.js
@@ -1,42 +1,18 @@
-/* global addEventListener, Response */
+const fetch = require("node-fetch");
 
-addEventListener('fetch', (event) => {
-  event.respondWith(
-    handleRequest(event.request).catch(
-      (err) => new Response(err.message, { status: 500 })
-    )
-  )
-})
+const API_ENDPOINT ='https://www.youtube.com/channel/UCt4t-jeY85JegMlZ-E5UWtA/live';
 
-async function handleRequest (request) {
-  const { pathname } = new URL(request.url)
-
-  if (pathname.startsWith('/channel/')) {
-    // supported api /channel/:id.m3u8
-    const channel = pathname.split('/')?.[2]?.split('.')?.[0]
-
-    if (channel !== '') {
-      const url = `https://www.youtube.com/channel/${channel}/live`
-
-      const response = await fetch(url, {
-        cf: {
-          cacheTtl: 10800, // 3 hours
-          cacheEverything: true
-        }
-      })
-
-      if (response.ok) {
-        const text = await response.text()
-        const stream = text.match(/(?<=hlsManifestUrl":").*\.m3u8/g)
-
-        return Response.redirect(stream, 302)
-      } else {
-        throw Error(`Youtube URL (${url}) failed with status: ${response.status}`)
-      }
-    } else {
-      throw Error(`Channel ID not found: ${pathname}`)
-    }
-  } else {
-    throw Error(`Path not found: ${pathname} `)
+exports.handler = async (event, context) => {
+  try {
+    const response = await fetch(API_ENDPOINT);
+	const text = await response.text()
+	const stream = text.match(/(?<=hlsManifestUrl":").*\.m3u8/g)
+    return { statusCode: 302, body: stream };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed fetching data' }),
+    };
   }
-}
+};
